439d5bc27a8f6007c924207cdf59ee5abe710533
David Glading <davidjglading@gmail.com>
2020-09-27 20:29:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 // writer code below!
 function happyHolidays()  {
   return "Happy holidays!"
-  console.log("Happy holidays!")
 }
 
 function happyHolidaysTo(name)  {
   var name = "you"
   return(`Happy holidays, ${name}!`)
-  console.log("Happy holidays, you!")
 }
 
 function happyCustomHolidayTo(holiday, name)  {
   var holiday = "Independence Day"
   var name = "you"
   return "Happy Independence Day, you"
-  console.log(`Happy ${holiday}, ${name}`)
 }
 
 function holidayCountdown(holiday, days)  {
   var holiday = "Mother's Day"
   var days = "20"
   return "It's 20 days until Mother's Day!"
-  console.log(`It's ${days} days until ${holiday}!`)
 }
